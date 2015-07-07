96a7d72e7576b4792dc197773877e123ae016b3d
Alexander Nadeau <wareya@gmail.com>
2015-07-07 15:47:48 -0400

    forgot this

--- gmdll.sh
+++ gmdll.sh
@@ -1,2 +1,2 @@
 i686-w64-mingw32-g++ -DGAME_MAKER -std=c++11 `echo *.cpp|sed s/newmain.cpp//` `i686-w64-mingw32-pkg-config opusfile sdl2 --static --libs --cflags|sed s/-XCClinker//` -static -o crossgm.dll \
--Wall -pedantic -Wno-unused -Wno-sign-compare -Wno-return-type -Wfatal-errors -mconsole -shared
+-s -Wall -pedantic -Wno-unused -Wno-sign-compare -Wno-return-type -Wfatal-errors -mconsole -shared
