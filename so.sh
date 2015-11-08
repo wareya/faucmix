a25677b02d7e3ae2e6205eb18ec9e302a69b648d
wareya <wareya@gmail.com>
2015-11-08 15:22:50 -0500

    Fix so generation script

--- so.sh
+++ so.sh
@@ -1,5 +1,5 @@
-cat `echo src/*.cpp|sed s/newmain.cpp//` | \
-g++ -shared -fPIC -std=c++11 -x c++ -Isrc \
+cat `echo src/*.cpp|sed s-src/newmain.cpp--` | \
+g++ -shared -fPIC -std=c++11 -x c++ - -Isrc \
 `pkg-config opusfile sdl2 --libs --cflags|sed s/-XCClinker//` \
 -s -g -ggdb \
 -Wall -pedantic -Wno-unused -Wno-sign-compare -Wno-return-type -Wfatal-errors \
