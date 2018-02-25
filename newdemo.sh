d340172e9e427570256cb2ca97afdfa8a5b17332
wareya <wareya@gmail.com>
2018-02-25 04:18:55 -0500

    fix shell script for small compile output

--- newdemo.sh
+++ newdemo.sh
@@ -6,5 +6,6 @@ g++ -DNO_OPUS -std=c++14 src/*.cpp \
 -static-libstdc++ \
 -lavrt -lole32 -lksuser -ldsound -ldxguid \
 -mwindows -mconsole \
+-Os -ffunction-sections -flto -fwhole-program -Wl,--gc-sections -Wl,--strip-all \
 -o fauxmix.exe
 #ar rvs libfauxmix.a fauxmix.o
